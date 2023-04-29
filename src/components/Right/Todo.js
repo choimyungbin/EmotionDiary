ca2432506544d47ea44826c5cb32de47e2e68a7e
choimyungbin <bbin852456.bbc@gmail.com>
2023-04-30 06:52:32 +0900

    added todo-list

--- src/components/Right/Todo.js
+++ src/components/Right/Todo.js
@@ -1,32 +1,63 @@
-import React, { useState } from 'react';
-const Todo = ({ onAddTodo }) => {
+import React, { useState, useEffect } from 'react';
+const Todo = () => {
+  const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState('');
-  const handleTodo = (e) => {
+  // const TODOS_KEY = 'todos';
+  // const saveTodos = () => {
+  //   localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+  // };
+  const changeTodos = (newTodo) => {
+    setTodos([newTodo, ...todos]);
+  };
+  const handleSubmit = (e) => {
     e.preventDefault();
     const newTodo = {
       id: Date.now(),
       text: todo,
     };
+    changeTodos(newTodo);
     setTodo('');
-    onAddTodo(newTodo);
-    return newTodo;
+
+    // saveTodos();
   };
+  useEffect(() => {}, [changeTodos]);
   const handleTextChange = (e) => {
     setTodo(e.target.value);
   };
+
+  const handleDelete = (e) => {
+    const li = e.target.parentElement;
+    setTodos(todos.filter((todo) => todo.id !== parseInt(li.id)));
+    li.remove();
+    // saveTodos();
+  };
+
+  // const savedTodos = localStorage.getItem(TODOS_KEY);
+  // if (savedTodos !== null) {
+  //   const parsedTodos = JSON.parse(savedTodos);
+  //   setTodos(parsedTodos);
+  // }
+
   return (
     <div id='todo-div'>
       <h1>To-do List</h1>
-      <form id='todo-form' onSubmit={handleTodo}>
+      <form id='todo-form' onSubmit={handleSubmit}>
         <input
           type='text'
           value={todo}
           onChange={handleTextChange}
-          placeholder='Write a Todo and Press Enter'
+          placeholder='Press Enter to submit'
           required
         />
       </form>
-      <ul id='todo-list'></ul>
+      <ul id='todo-list'>
+        {todos.map((it) => (
+          <li key={it.id}>
+            {it.text}
+            <button onClick={handleDelete}>삭제</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
